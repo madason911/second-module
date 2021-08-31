0d83567f074a1e87af32a898543a35f6e7a7d635
madason911 <31772534+madason911@users.noreply.github.com>
2021-08-31 12:56:48 +0300

    first-project

---
+++ 1-project/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const item = (
+  <h1> Hello world меня зовут: Асирдинов Гаджимурад Магомедрасулович </h1>
+);
+ReactDOM.render(item, document.querySelector("#root"));
